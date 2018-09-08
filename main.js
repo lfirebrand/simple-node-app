d9a79f034e82ede44295566ab02b2f20c90be9c3
Lexi Zgud <lexibitesblog@gmail.com>
2018-09-08 15:42:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const greetings = require("./greeting.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Hallo"
+greetings.greetingIcelandic();
+
+const maths = require("./math.js");
+
+// returns 20
+maths.multiplyVariables(5, 4);
+
+// returns 9
+maths.addVariables(5, 4);
+
+// returns 40
+maths.multiplyVariables(10, 4);
+
+// returns 14
+maths.addVariables(10, 4);
---
+++ math.js
@@ -0,0 +1,9 @@
+exports.multiplyVariables = function(a, b){
+  var ans = a * b;
+  console.log(ans);
+}
+
+exports.addVariables = function(a, b){
+  var sum = a + b;
+  console.log(sum);
+}
